4be8518e9a83a5be38c8ca786bd9a87abb022462
sams4566 <sams4566@gmail.com>
2022-03-14 18:00:21 +0000

    Initial Commit

---
+++ src/main/java/com/sparta/ss/bubblesort/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.sparta.ss.bubblesort;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.Scanner;
+
+public class BubbleSort {
+    static void bubbleSort(int[] numbers) {
+        int len = numbers.length;
+        int a = 0;
+        for (int i = 0; i < len; i++) {
+            for (int j = 1; j < (len - i); j++) {
+                if (numbers[j - 1] > numbers[j]) {
+                    a = numbers[j - 1];
+                    numbers[j - 1] = numbers[j];
+                    numbers[j] = a;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int scanned = sc.nextInt();
+        int[] numbers = new int[scanned];
+        Random random = new Random();
+        for (int i = 0; i < scanned; i++) {
+            numbers[i] = random.nextInt(1000);
+        }
+        bubbleSort(numbers);
+        System.out.println(Arrays.toString(numbers));
+    }
+}
